cf2aabd9b19c8a694ec207dd2fed8fabce390fe8
claudfelix <60541334+claudfelix@users.noreply.github.com>
2020-02-04 22:43:02 +0800

    Add files via upload

---
+++ Calculator - VB.net/Calculator - VB.net/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.displayScreen = New System.Windows.Forms.TextBox()
+        Me.buttonBackspace = New System.Windows.Forms.Button()
+        Me.buttonSeven = New System.Windows.Forms.Button()
+        Me.buttonFour = New System.Windows.Forms.Button()
+        Me.buttonOne = New System.Windows.Forms.Button()
+        Me.buttonZero = New System.Windows.Forms.Button()
+        Me.buttonPoint = New System.Windows.Forms.Button()
+        Me.buttonTwo = New System.Windows.Forms.Button()
+        Me.buttonFive = New System.Windows.Forms.Button()
+        Me.buttonEight = New System.Windows.Forms.Button()
+        Me.buttonCE = New System.Windows.Forms.Button()
+        Me.buttonEquals = New System.Windows.Forms.Button()
+        Me.buttonThree = New System.Windows.Forms.Button()
+        Me.button6 = New System.Windows.Forms.Button()
+        Me.buttonNine = New System.Windows.Forms.Button()
+        Me.buttonC = New System.Windows.Forms.Button()
+        Me.buttonMultiply = New System.Windows.Forms.Button()
+        Me.buttonDivide = New System.Windows.Forms.Button()
+        Me.buttonMinus = New System.Windows.Forms.Button()
+        Me.buttonPlus = New System.Windows.Forms.Button()
+        Me.buttonPlusMinus = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'displayScreen
+        '
+        Me.displayScreen.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.displayScreen.Location = New System.Drawing.Point(12, 12)
+        Me.displayScreen.Multiline = True
+        Me.displayScreen.Name = "displayScreen"
+        Me.displayScreen.Size = New System.Drawing.Size(418, 74)
+        Me.displayScreen.TabIndex = 0
+        Me.displayScreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'buttonBackspace
+        '
+        Me.buttonBackspace.BackColor = System.Drawing.Color.Transparent
+        Me.buttonBackspace.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonBackspace.Location = New System.Drawing.Point(12, 92)
+        Me.buttonBackspace.Name = "buttonBackspace"
+        Me.buttonBackspace.Size = New System.Drawing.Size(100, 78)
+        Me.buttonBackspace.TabIndex = 1
+        Me.buttonBackspace.Text = "⌫"
+        Me.buttonBackspace.UseVisualStyleBackColor = False
+        '
+        'buttonSeven
+        '
+        Me.buttonSeven.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonSeven.Location = New System.Drawing.Point(12, 176)
+        Me.buttonSeven.Name = "buttonSeven"
+        Me.buttonSeven.Size = New System.Drawing.Size(100, 78)
+        Me.buttonSeven.TabIndex = 2
+        Me.buttonSeven.Text = "7"
+        Me.buttonSeven.UseVisualStyleBackColor = True
+        '
+        'buttonFour
+        '
+        Me.buttonFour.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonFour.Location = New System.Drawing.Point(12, 260)
+        Me.buttonFour.Name = "buttonFour"
+        Me.buttonFour.Size = New System.Drawing.Size(100, 78)
+        Me.buttonFour.TabIndex = 3
+        Me.buttonFour.Text = "4"
+        Me.buttonFour.UseVisualStyleBackColor = True
+        '
+        'buttonOne
+        '
+        Me.buttonOne.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonOne.Location = New System.Drawing.Point(12, 344)
+        Me.buttonOne.Name = "buttonOne"
+        Me.buttonOne.Size = New System.Drawing.Size(100, 78)
+        Me.buttonOne.TabIndex = 4
+        Me.buttonOne.Text = "1"
+        Me.buttonOne.UseVisualStyleBackColor = True
+        '
+        'buttonZero
+        '
+        Me.buttonZero.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonZero.Location = New System.Drawing.Point(12, 427)
+        Me.buttonZero.Name = "buttonZero"
+        Me.buttonZero.Size = New System.Drawing.Size(100, 78)
+        Me.buttonZero.TabIndex = 5
+        Me.buttonZero.Text = "0"
+        Me.buttonZero.UseVisualStyleBackColor = True
+        '
+        'buttonPoint
+        '
+        Me.buttonPoint.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonPoint.Location = New System.Drawing.Point(118, 427)
+        Me.buttonPoint.Name = "buttonPoint"
+        Me.buttonPoint.Size = New System.Drawing.Size(100, 78)
+        Me.buttonPoint.TabIndex = 10
+        Me.buttonPoint.Text = "."
+        Me.buttonPoint.UseVisualStyleBackColor = True
+        '
+        'buttonTwo
+        '
+        Me.buttonTwo.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonTwo.Location = New System.Drawing.Point(118, 344)
+        Me.buttonTwo.Name = "buttonTwo"
+        Me.buttonTwo.Size = New System.Drawing.Size(100, 78)
+        Me.buttonTwo.TabIndex = 9
+        Me.buttonTwo.Text = "2"
+        Me.buttonTwo.UseVisualStyleBackColor = True
+        '
+        'buttonFive
+        '
+        Me.buttonFive.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonFive.Location = New System.Drawing.Point(118, 260)
+        Me.buttonFive.Name = "buttonFive"
+        Me.buttonFive.Size = New System.Drawing.Size(100, 78)
+        Me.buttonFive.TabIndex = 8
+        Me.buttonFive.Text = "5"
+        Me.buttonFive.UseVisualStyleBackColor = True
+        '
+        'buttonEight
+        '
+        Me.buttonEight.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonEight.Location = New System.Drawing.Point(118, 176)
+        Me.buttonEight.Name = "buttonEight"
+        Me.buttonEight.Size = New System.Drawing.Size(100, 78)
+        Me.buttonEight.TabIndex = 7
+        Me.buttonEight.Text = "8"
+        Me.buttonEight.UseVisualStyleBackColor = True
+        '
+        'buttonCE
+        '
+        Me.buttonCE.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonCE.Location = New System.Drawing.Point(118, 92)
+        Me.buttonCE.Name = "buttonCE"
+        Me.buttonCE.Size = New System.Drawing.Size(100, 78)
+        Me.buttonCE.TabIndex = 6
+        Me.buttonCE.Text = "CE"
+        Me.buttonCE.UseVisualStyleBackColor = True
+        '
+        'buttonEquals
+        '
+        Me.buttonEquals.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonEquals.Location = New System.Drawing.Point(224, 427)
+        Me.buttonEquals.Name = "buttonEquals"
+        Me.buttonEquals.Size = New System.Drawing.Size(100, 78)
+        Me.buttonEquals.TabIndex = 15
+        Me.buttonEquals.Text = "="
+        Me.buttonEquals.UseVisualStyleBackColor = True
+        '
+        'buttonThree
+        '
+        Me.buttonThree.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonThree.Location = New System.Drawing.Point(224, 344)
+        Me.buttonThree.Name = "buttonThree"
+        Me.buttonThree.Size = New System.Drawing.Size(100, 78)
+        Me.buttonThree.TabIndex = 14
+        Me.buttonThree.Text = "3"
+        Me.buttonThree.UseVisualStyleBackColor = True
+        '
+        'button6
+        '
+        Me.button6.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button6.Location = New System.Drawing.Point(224, 260)
+        Me.button6.Name = "button6"
+        Me.button6.Size = New System.Drawing.Size(100, 78)
+        Me.button6.TabIndex = 13
+        Me.button6.Text = "6"
+        Me.button6.UseVisualStyleBackColor = True
+        '
+        'buttonNine
+        '
+        Me.buttonNine.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonNine.Location = New System.Drawing.Point(224, 176)
+        Me.buttonNine.Name = "buttonNine"
+        Me.buttonNine.Size = New System.Drawing.Size(100, 78)
+        Me.buttonNine.TabIndex = 12
+        Me.buttonNine.Text = "9"
+        Me.buttonNine.UseVisualStyleBackColor = True
+        '
+        'buttonC
+        '
+        Me.buttonC.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonC.Location = New System.Drawing.Point(224, 92)
+        Me.buttonC.Name = "buttonC"
+        Me.buttonC.Size = New System.Drawing.Size(100, 78)
+        Me.buttonC.TabIndex = 11
+        Me.buttonC.Text = "C"
+        Me.buttonC.UseVisualStyleBackColor = True
+        '
+        'buttonMultiply
+        '
+        Me.buttonMultiply.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonMultiply.Location = New System.Drawing.Point(330, 427)
+        Me.buttonMultiply.Name = "buttonMultiply"
+        Me.buttonMultiply.Size = New System.Drawing.Size(100, 78)
+        Me.buttonMultiply.TabIndex = 20
+        Me.buttonMultiply.Text = "*"
+        Me.buttonMultiply.UseVisualStyleBackColor = True
+        '
+        'buttonDivide
+        '
+        Me.buttonDivide.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonDivide.Location = New System.Drawing.Point(330, 344)
+        Me.buttonDivide.Name = "buttonDivide"
+        Me.buttonDivide.Size = New System.Drawing.Size(100, 78)
+        Me.buttonDivide.TabIndex = 19
+        Me.buttonDivide.Text = "/"
+        Me.buttonDivide.UseVisualStyleBackColor = True
+        '
+        'buttonMinus
+        '
+        Me.buttonMinus.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonMinus.Location = New System.Drawing.Point(330, 260)
+        Me.buttonMinus.Name = "buttonMinus"
+        Me.buttonMinus.Size = New System.Drawing.Size(100, 78)
+        Me.buttonMinus.TabIndex = 18
+        Me.buttonMinus.Text = "-"
+        Me.buttonMinus.UseVisualStyleBackColor = True
+        '
+        'buttonPlus
+        '
+        Me.buttonPlus.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonPlus.Location = New System.Drawing.Point(330, 176)
+        Me.buttonPlus.Name = "buttonPlus"
+        Me.buttonPlus.Size = New System.Drawing.Size(100, 78)
+        Me.buttonPlus.TabIndex = 17
+        Me.buttonPlus.Text = "+"
+        Me.buttonPlus.UseVisualStyleBackColor = True
+        '
+        'buttonPlusMinus
+        '
+        Me.buttonPlusMinus.Font = New System.Drawing.Font("Palatino Linotype", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonPlusMinus.Location = New System.Drawing.Point(330, 92)
+        Me.buttonPlusMinus.Name = "buttonPlusMinus"
+        Me.buttonPlusMinus.Size = New System.Drawing.Size(100, 78)
+        Me.buttonPlusMinus.TabIndex = 16
+        Me.buttonPlusMinus.Text = "±"
+        Me.buttonPlusMinus.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(438, 512)
+        Me.Controls.Add(Me.buttonMultiply)
+        Me.Controls.Add(Me.buttonDivide)
+        Me.Controls.Add(Me.buttonMinus)
+        Me.Controls.Add(Me.buttonPlus)
+        Me.Controls.Add(Me.buttonPlusMinus)
+        Me.Controls.Add(Me.buttonEquals)
+        Me.Controls.Add(Me.buttonThree)
+        Me.Controls.Add(Me.button6)
+        Me.Controls.Add(Me.buttonNine)
+        Me.Controls.Add(Me.buttonC)
+        Me.Controls.Add(Me.buttonPoint)
+        Me.Controls.Add(Me.buttonTwo)
+        Me.Controls.Add(Me.buttonFive)
+        Me.Controls.Add(Me.buttonEight)
+        Me.Controls.Add(Me.buttonCE)
+        Me.Controls.Add(Me.buttonZero)
+        Me.Controls.Add(Me.buttonOne)
+        Me.Controls.Add(Me.buttonFour)
+        Me.Controls.Add(Me.buttonSeven)
+        Me.Controls.Add(Me.buttonBackspace)
+        Me.Controls.Add(Me.displayScreen)
+        Me.Name = "Form1"
+        Me.Text = "VB.net - Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents displayScreen As TextBox
+    Friend WithEvents buttonBackspace As Button
+    Friend WithEvents buttonSeven As Button
+    Friend WithEvents buttonFour As Button
+    Friend WithEvents buttonOne As Button
+    Friend WithEvents buttonZero As Button
+    Friend WithEvents buttonPoint As Button
+    Friend WithEvents buttonTwo As Button
+    Friend WithEvents buttonFive As Button
+    Friend WithEvents buttonEight As Button
+    Friend WithEvents buttonCE As Button
+    Friend WithEvents buttonEquals As Button
+    Friend WithEvents buttonThree As Button
+    Friend WithEvents button6 As Button
+    Friend WithEvents buttonNine As Button
+    Friend WithEvents buttonC As Button
+    Friend WithEvents buttonMultiply As Button
+    Friend WithEvents buttonDivide As Button
+    Friend WithEvents buttonMinus As Button
+    Friend WithEvents buttonPlus As Button
+    Friend WithEvents buttonPlusMinus As Button
+End Class
---
+++ Calculator - VB.net/Calculator - VB.net/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+
+    Dim firstValue, secondValue, result As Double
+    Dim op As String
+    Private Sub Number_Click(sender As Object, e As EventArgs) Handles buttonZero.Click, buttonTwo.Click, buttonThree.Click, buttonSeven.Click, buttonPoint.Click, buttonOne.Click, buttonNine.Click, buttonFour.Click, buttonFive.Click, buttonEight.Click, button6.Click
+        Dim b As Button = sender
+
+        If (displayScreen.Text = "0") Then
+            displayScreen.Text = ""
+            displayScreen.Text = b.Text
+            displayScreen.Text = b.Text
+
+        ElseIf (b.Text = ".") Then
+            If (Not displayScreen.Text.Contains(".")) Then
+                displayScreen.Text = displayScreen.Text + b.Text
+            End If
+
+        Else
+            displayScreen.Text = displayScreen.Text + b.Text
+        End If
+    End Sub
+
+    Private Sub buttonEquals_Click(sender As Object, e As EventArgs) Handles buttonEquals.Click
+        secondValue = displayScreen.Text
+
+        If op = "+" Then
+            result = firstValue + secondValue
+            displayScreen.Text = result
+
+        ElseIf op = "-" Then
+            result = firstValue - secondValue
+            displayScreen.Text = result
+
+        ElseIf op = "*" Then
+            result = firstValue - secondValue
+            displayScreen.Text = result
+
+        ElseIf op = "/" Then
+            result = firstValue - secondValue
+            displayScreen.Text = result
+        End If
+    End Sub
+
+    Private Sub buttonPlusMinus_Click(sender As Object, e As EventArgs) Handles buttonPlusMinus.Click
+        If (displayScreen.Text.Contains("-")) Then
+            displayScreen.Text = displayScreen.Text.Remove(0, 1)
+        Else
+            displayScreen.Text = "-" + displayScreen.Text
+        End If
+    End Sub
+
+    Private Sub buttonC_Click(sender As Object, e As EventArgs) Handles buttonC.Click
+        displayScreen.Text = "0"
+    End Sub
+
+    Private Sub buttonCE_Click(sender As Object, e As EventArgs) Handles buttonCE.Click
+        displayScreen.Text = "0"
+    End Sub
+
+    Private Sub buttonBackspace_Click(sender As Object, e As EventArgs) Handles buttonBackspace.Click
+        If (displayScreen.Text.Length > 0) Then
+            displayScreen.Text = displayScreen.Text.Remove(displayScreen.Text.Length - 1, 1)
+        End If
+        If (displayScreen.Text = "") Then
+            displayScreen.Text = "0"
+        End If
+    End Sub
+
+    Private Sub Operator_Click(sender As Object, e As EventArgs) Handles buttonPlus.Click, buttonMultiply.Click, buttonMinus.Click, buttonDivide.Click
+        Dim b As Button = sender
+
+        firstValue = displayScreen.Text
+        op = b.Text
+        displayScreen.Text = ""
+    End Sub
+End Class
